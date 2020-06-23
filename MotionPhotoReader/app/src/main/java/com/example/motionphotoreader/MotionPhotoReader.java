11bd5c7a39a82cf5cfccbf55b7a8f1aaedb020cf
sxzhang25 <zsharonx@gmail.com>
2020-06-22 20:59:00 -0400

    Write Javadocs summarizing method purposes.

--- MotionPhotoReader/app/src/main/java/com/example/motionphotoreader/MotionPhotoReader.java
+++ MotionPhotoReader/app/src/main/java/com/example/motionphotoreader/MotionPhotoReader.java
@@ -17,26 +17,56 @@ public class MotionPhotoReader {
         this.surface = surface;
     }
 
+    /**
+     * Opens and prepares a new MotionPhotoReader for a particular file.
+     */
     public static MotionPhotoReader open(String filename, Surface surface) {
         return null;
     }
 
+    /**
+     * Parses the XMP in the file to find the microvideo offset, and sets up the MediaCodec
+     * extractors and decoders.
+     */
     private void prepare() {
 
     }
 
+    /**
+     * Shut down all resources allocated to the MotionPhotoReader instance.
+     */
     public void close() {
 
     }
 
+    /**
+     * Checks whether the Motion Photo video has a succeeding frame.
+     * @return 1 if there is no frame, 0 if the next frame exists, and -1 if no buffers are available.
+     */
     public int hasNextFrame() {
         return -1;
     }
 
+    /**
+     * Advances the decoder and extractor by one frame.
+     */
     public void nextFrame() {
 
     }
 
+    /**
+     * Sets the decoder and extractor to the frame specified by the given timestamp.
+     * @param timeUs The desired timestamp of the video.
+     * @param mode The sync mode of the extractor.
+     */
+    public void seekTo(long timeUs, int mode) {
+
+    }
+
+    /**
+     * The XmpParser class is a child class intended to help extract the microvideo offset information
+     * from the XMP metadata of the given Motion Photo. This class is for internal use only.
+     */
     private static class XmpParser {
 
     }
