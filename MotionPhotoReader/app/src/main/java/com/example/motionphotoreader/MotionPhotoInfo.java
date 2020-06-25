5f086c6c7602a4e92c86f09baf64f45a3fdec8d2
sxzhang25 <zsharonx@gmail.com>
2020-06-25 11:47:06 -0400

    Add relevant motion photo info metadata fields.

--- MotionPhotoReader/app/src/main/java/com/example/motionphotoreader/MotionPhotoInfo.java
+++ MotionPhotoReader/app/src/main/java/com/example/motionphotoreader/MotionPhotoInfo.java
@@ -1,9 +1,48 @@
 package com.example.motionphotoreader;
 
+import android.util.Log;
+
+import com.adobe.internal.xmp.XMPException;
+import com.adobe.internal.xmp.XMPMeta;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 /**
  * Holds data about the Motion Photo file.
  */
 public class MotionPhotoInfo {
 
+    private String filename;
+    private int width;
+    private int height;
+    private int videoOffset;
+    private int xResolution;
+    private int yResolution;
 
+    /**
+     * Returns the XMP metadata associated with a given file.
+     */
+    public MotionPhotoInfo(String filename) {
+    }
+
+    public int getWidth() {
+        return -1;
+    }
+
+    public int getHeight() {
+        return -1;
+    }
+
+    public int getVideoOffset() {
+        return -1;
+    }
+
+    public int getXResolution() {
+        return -1;
+    }
+
+    public int getyResolution() {
+        return -1;
+    }
 }
