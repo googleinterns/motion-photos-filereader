46c17d8602957ec3da365e44f887433f91a4057e
sxzhang25 <zsharonx@gmail.com>
2020-06-25 14:38:42 -0400

    Extract video offset.

--- MotionPhotoReader/app/src/main/java/com/example/motionphotoreader/MotionPhotoInfo.java
+++ MotionPhotoReader/app/src/main/java/com/example/motionphotoreader/MotionPhotoInfo.java
@@ -1,10 +1,15 @@
 package com.example.motionphotoreader;
 
+import android.media.MediaMetadataRetriever;
+import android.net.Uri;
 import android.util.Log;
 
 import com.adobe.internal.xmp.XMPException;
 import com.adobe.internal.xmp.XMPMeta;
 
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
@@ -13,43 +18,27 @@ import java.io.IOException;
  */
 public class MotionPhotoInfo {
 
-    private String filename;
     private int width;
     private int height;
     private int videoOffset;
-    private int xResolution;
-    private int yResolution;
 
     /**
      * Returns the XMP metadata associated with a given file.
      */
     public MotionPhotoInfo(String filename) throws IOException, XMPException {
         XMPMeta meta = XmpParser.getXmpMetadata(filename);
-        this.filename = filename;
-        width = meta.getPropertyInteger("http://ns.google.com/photos/1.0/camera/", "ExifImageWidth");
-        height = meta.getPropertyInteger("http://ns.google.com/photos/1.0/camera/", "ExifImageHeight");
         videoOffset = meta.getPropertyInteger("http://ns.google.com/photos/1.0/camera/", "MicroVideoOffset");
-        xResolution = meta.getPropertyInteger("http://ns.google.com/photos/1.0/camera/", "XResolution");
-        yResolution = meta.getPropertyInteger("http://ns.google.com/photos/1.0/camera/", "YResolution");
     }
 
     public int getWidth() {
-        return -1;
+        return width;
     }
 
     public int getHeight() {
-        return -1;
+        return height;
     }
 
     public int getVideoOffset() {
-        return -1;
-    }
-
-    public int getXResolution() {
-        return -1;
-    }
-
-    public int getyResolution() {
-        return -1;
+        return videoOffset;
     }
 }
