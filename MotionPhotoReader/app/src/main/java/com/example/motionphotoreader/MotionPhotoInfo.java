85a48faeaeef67b9aa6a68172666de31308ff22c
sxzhang25 <zsharonx@gmail.com>
2020-06-25 22:10:58 -0400

    Add video duration as a field.

--- MotionPhotoReader/app/src/main/java/com/example/motionphotoreader/MotionPhotoInfo.java
+++ MotionPhotoReader/app/src/main/java/com/example/motionphotoreader/MotionPhotoInfo.java
@@ -1,8 +1,12 @@
 package com.example.motionphotoreader;
 
+import android.media.MediaExtractor;
+import android.media.MediaFormat;
 import android.media.MediaMetadataRetriever;
 import android.net.Uri;
 import android.util.Log;
+
+import androidx.annotation.VisibleForTesting;
 
 import com.adobe.internal.xmp.XMPException;
 import com.adobe.internal.xmp.XMPMeta;
@@ -20,14 +24,47 @@ public class MotionPhotoInfo {
 
     private int width;
     private int height;
+    private long duration;
     private int videoOffset;
 
+    private final MediaExtractor extractor;
+
     /**
-     * Returns the XMP metadata associated with a given file.
+     * Returns the MotionPhotoInfo associated with a given file.
      */
-    public MotionPhotoInfo(String filename) throws IOException, XMPException {
+    @VisibleForTesting
+    public MotionPhotoInfo(String filename, MediaExtractor extractor) throws IOException, XMPException {
+        this.extractor = extractor;
+
         XMPMeta meta = XmpParser.getXmpMetadata(filename);
         videoOffset = meta.getPropertyInteger("http://ns.google.com/photos/1.0/camera/", "MicroVideoOffset");
+
+        File f = new File(filename);
+        FileInputStream fileInputStream = new FileInputStream(f);
+        FileDescriptor fd = fileInputStream.getFD();
+        extractor.setDataSource(fd, f.length() - videoOffset, videoOffset);
+
+        // Find the video track and create an appropriate media decoder
+        for (int i = 0; i < extractor.getTrackCount(); i++) {
+            MediaFormat format = extractor.getTrackFormat(i);
+            String mime = format.getString(MediaFormat.KEY_MIME);
+            assert mime != null;
+            if (mime.startsWith("video/")) {
+                width = format.getInteger(MediaFormat.KEY_WIDTH);
+                height = format.getInteger(MediaFormat.KEY_HEIGHT);
+                duration = format.getLong(MediaFormat.KEY_DURATION);
+                break;
+            }
+        }
+
+        fileInputStream.close();
+    }
+
+    /**
+     * Returns a new instance of MotionPhotoInfo for a specified file.
+     */
+    public static MotionPhotoInfo newInstance(String filename) throws IOException, XMPException {
+        return new MotionPhotoInfo(filename, new MediaExtractor());
     }
 
     public int getWidth() {
@@ -40,5 +77,9 @@ public class MotionPhotoInfo {
 
     public int getVideoOffset() {
         return videoOffset;
+    }
+
+    public long getDuration() {
+        return duration;
     }
 }
