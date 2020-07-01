5c48ba24f178df1d43cbbf4e5aed0d2eebd41eb1
sxzhang25 <zsharonx@gmail.com>
2020-07-01 19:39:10 -0400

    Rename file.

--- MotionPhotoReader/app/src/test/java/com/example/motionphotoreader/MotionPhotoInfoTest.java
+++ MotionPhotoReader/app/src/test/java/com/example/motionphotoreader/MotionPhotoInfoTest.java
@@ -26,7 +26,7 @@ import static org.mockito.Mockito.when;
  * Example local unit test, which will execute on the development machine (host).
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class MotionPhotoInfoUnitTest {
+public class MotionPhotoInfoTest {
     private final static int KEY_WIDTH = 4032;
     private final static int KEY_HEIGHT = 3024;
     private final static long KEY_DURATION = 297168;
