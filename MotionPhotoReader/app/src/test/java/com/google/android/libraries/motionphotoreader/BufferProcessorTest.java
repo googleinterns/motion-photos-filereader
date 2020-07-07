87d283a3182d55703bcc4e57605b380f70a08b9a
sxzhang25 <zsharonx@gmail.com>
2020-07-07 09:59:18 -0400

    Update tests to reflect BufferProcessor changes.

--- MotionPhotoReader/app/src/test/java/com/google/android/libraries/motionphotoreader/BufferProcessorTest.java
+++ MotionPhotoReader/app/src/test/java/com/google/android/libraries/motionphotoreader/BufferProcessorTest.java
@@ -31,7 +31,7 @@ import static org.mockito.Mockito.verify;
 /**
  * Local unit test for the BufferHandler class.
  */
-public class BufferHandlerTest {
+public class BufferProcessorTest {
 
     private MediaExtractor lowResExtractor;
     private MediaCodec lowResDecoder;
@@ -57,21 +57,16 @@ public class BufferHandlerTest {
 
     @Test
     public void handleNextFrameMsg_hasNextFrameTrue_isCorrect() {
-        BufferHandler bufferHandler = spy(new BufferHandler(
-                mock(Looper.class),
-                lowResExtractor, lowResDecoder,
-                availableInputBuffers, availableOutputBuffers
+        BufferProcessor bufferProcessor = spy(new BufferProcessor(
+                lowResExtractor, lowResDecoder, availableInputBuffers, availableOutputBuffers
         ));
 
         doAnswer((Answer<Integer>) invocation -> 16).when(lowResExtractor).readSampleData(any(ByteBuffer.class), anyInt());
         doAnswer((Answer<ByteBuffer>) invocation -> mock(ByteBuffer.class)).when(lowResDecoder).getInputBuffer(anyInt());
 
-        Message message = mock(Message.class);
         Bundle messageData = mock(Bundle.class);
-
-        doAnswer((Answer<Bundle>) invocation -> messageData).when(message).getData();
         doAnswer((Answer<Integer>) invocation -> MotionPhotoReader.MSG_NEXT_FRAME).when(messageData).getInt(anyString());
-        bufferHandler.handleMessage(message);
+        bufferProcessor.handleBundle(messageData);
 
         verify(lowResExtractor).readSampleData(any(ByteBuffer.class), eq(0));
         verify(lowResDecoder, times(1)).queueInputBuffer(eq(1), eq(0), anyInt(), anyLong(), eq(0));
@@ -82,21 +77,16 @@ public class BufferHandlerTest {
 
     @Test
     public void handleNextFrameMsg_hasNextFrameFalse_isCorrect() {
-        BufferHandler bufferHandler = spy(new BufferHandler(
-                mock(Looper.class),
-                lowResExtractor, lowResDecoder,
-                availableInputBuffers, availableOutputBuffers
+        BufferProcessor bufferProcessor = spy(new BufferProcessor(
+                lowResExtractor, lowResDecoder, availableInputBuffers, availableOutputBuffers
         ));
 
         doAnswer((Answer<Integer>) invocation -> -1).when(lowResExtractor).readSampleData(any(ByteBuffer.class), anyInt());
         doAnswer((Answer<ByteBuffer>) invocation -> mock(ByteBuffer.class)).when(lowResDecoder).getInputBuffer(anyInt());
 
-        Message message = mock(Message.class);
         Bundle messageData = mock(Bundle.class);
-
-        doAnswer((Answer<Bundle>) invocation -> messageData).when(message).getData();
         doAnswer((Answer<Integer>) invocation -> MotionPhotoReader.MSG_NEXT_FRAME).when(messageData).getInt(anyString());
-        bufferHandler.handleMessage(message);
+        bufferProcessor.handleBundle(messageData);
 
         verify(lowResExtractor).readSampleData(any(ByteBuffer.class), anyInt());
         verify(lowResDecoder, times(1)).queueInputBuffer(eq(1), eq(0), eq(0), eq(0L), eq(MediaCodec.BUFFER_FLAG_END_OF_STREAM));
@@ -106,18 +96,13 @@ public class BufferHandlerTest {
 
     @Test
     public void handleSeekToFrameMsg_isCorrect() {
-        BufferHandler bufferHandler = spy(new BufferHandler(
-                mock(Looper.class),
-                lowResExtractor, lowResDecoder,
-                availableInputBuffers, availableOutputBuffers
+        BufferProcessor bufferProcessor = spy(new BufferProcessor(
+                lowResExtractor, lowResDecoder, availableInputBuffers, availableOutputBuffers
         ));
 
-        Message message = mock(Message.class);
         Bundle messageData = mock(Bundle.class);
-
-        doAnswer((Answer<Bundle>) invocation -> messageData).when(message).getData();
         doAnswer((Answer<Integer>) invocation -> MotionPhotoReader.MSG_SEEK_TO_FRAME).when(messageData).getInt(anyString());
-        bufferHandler.handleMessage(message);
+        bufferProcessor.handleBundle(messageData);
 
         verify(lowResExtractor, times(1)).seekTo(anyLong(), anyInt());
         verify(lowResDecoder).releaseOutputBuffer(anyInt(), anyLong());
@@ -125,18 +110,13 @@ public class BufferHandlerTest {
 
     @Test(expected = IllegalStateException.class)
     public void handleIncorrectMsg_isCorrect() {
-        BufferHandler bufferHandler = spy(new BufferHandler(
-                mock(Looper.class),
-                lowResExtractor, lowResDecoder,
-                availableInputBuffers, availableOutputBuffers
+        BufferProcessor bufferProcessor = spy(new BufferProcessor(
+                lowResExtractor, lowResDecoder, availableInputBuffers, availableOutputBuffers
         ));
 
-        Message message = mock(Message.class);
         Bundle messageData = mock(Bundle.class);
-
-        doAnswer((Answer<Bundle>) invocation -> messageData).when(message).getData();
         doAnswer((Answer<Integer>) invocation -> 0x0100).when(messageData).getInt(anyString());
-        bufferHandler.handleMessage(message);
+        bufferProcessor.handleBundle(messageData);
 
         verify(lowResExtractor, never()).seekTo(anyLong(), anyInt());
         verify(lowResDecoder, never()).releaseOutputBuffer(anyInt(), anyLong());
