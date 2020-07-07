7fa7573abc2b0046d5a79e7c9b769da5af66880d
sxzhang25 <zsharonx@gmail.com>
2020-07-07 11:03:48 -0400

    Add unit tests for XmpParser class.

---
+++ MotionPhotoReader/app/src/test/java/com/google/android/libraries/motionphotoreader/XmpParserTest.java
@@ -0,0 +1,44 @@
+package com.google.android.libraries.motionphotoreader;
+
+import com.adobe.internal.xmp.XMPException;
+import com.adobe.internal.xmp.XMPMeta;
+import com.google.common.primitives.Bytes;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+
+public class XmpParserTest {
+    private static final byte[] OPEN_ARR = "<x:xmpmeta".getBytes();  /* Start of XMP metadata tag */
+    private static final byte[] CLOSE_ARR = "</x:xmpmeta>".getBytes();  /* End of XMP metadata tag */
+
+    private String filename;
+
+    @Before
+    public void setUp() {
+        this.filename = this.getClass().getClassLoader().getResource("test_photo.jpg").getFile();
+    }
+
+    @Test
+    public void getXmpMetadata_isNotNull() throws IOException, XMPException {
+        XMPMeta meta = XmpParser.getXmpMetadata(filename);
+        assertNotNull(meta);
+    }
+
+    @Test
+    public void getXmpByteArray_hasCorrectHeader() throws IOException {
+        byte[] segArr = XmpParser.getXmpByteArray(filename);
+        int index = Bytes.indexOf(segArr, OPEN_ARR);
+        assertEquals(0, index);
+    }
+
+    @Test
+    public void getXmpByteArray_hasCorrectFooter() throws IOException {
+        byte[] segArr = XmpParser.getXmpByteArray(filename);
+        int index = Bytes.indexOf(segArr, CLOSE_ARR);
+        assertEquals(segArr.length, index + CLOSE_ARR.length);
+    }
+}
