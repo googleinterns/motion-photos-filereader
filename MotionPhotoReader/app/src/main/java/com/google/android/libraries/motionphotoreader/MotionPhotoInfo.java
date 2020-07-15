648ffbf2dbfc1a062a910a497ca9bf772e8052a1
sxzhang25 <zsharonx@gmail.com>
2020-07-15 11:47:16 -0400

    Add support for getting video offset from motion photo v2 file formats.
    Parse through all XMP nodes in the v2 XMP metadata and check if the nodes belong to the Directory array. If the property for the node matches the desired length or padding property paths, then add those values to the video offset.
    
    Also, updated unit tests to reflect additional v2 format and refactored setUp() for modularity.

--- MotionPhotoReader/app/src/main/java/com/google/android/libraries/motionphotoreader/MotionPhotoInfo.java
+++ MotionPhotoReader/app/src/main/java/com/google/android/libraries/motionphotoreader/MotionPhotoInfo.java
@@ -1,43 +1,54 @@
 package com.google.android.libraries.motionphotoreader;
 
-import android.media.MediaCodec;
 import android.media.MediaExtractor;
 import android.media.MediaFormat;
 import android.os.Build;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.annotation.VisibleForTesting;
 
 import com.adobe.internal.xmp.XMPException;
+import com.adobe.internal.xmp.XMPIterator;
 import com.adobe.internal.xmp.XMPMeta;
+import com.adobe.internal.xmp.options.PropertyOptions;
+import com.adobe.internal.xmp.properties.XMPProperty;
+import com.adobe.internal.xmp.properties.XMPPropertyInfo;
 
 import java.io.File;
 import java.io.FileDescriptor;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.sql.Struct;
+import java.util.IllegalFormatException;
 
 /**
  * Contains information relevant to extracting frames in a Motion Photo file.
  */
 public class MotionPhotoInfo {
 
-    private static final String CAMERA_XMP_NAMESPACE = "http://ns.google.com/photos/1.0/camera/";
+    private final static String TAG = "MotionPhotoInfo";
+
+    private static int MOTION_PHOTO_VERSION_V1 = 1;
+    private static int MOTION_PHOTO_VERSION_V2 = 2;
+
+    private static final String CAMERA_XMP_NAMESPACE =
+            "http://ns.google.com/photos/1.0/camera/";
+    private static final String CONTAINER_XMP_NAMESPACE =
+            "http://ns.google.com/photos/1.0/container/";
 
     private final int width;
     private final int height;
     private final long duration;
     private final int rotation;
     private final int videoOffset;
-    private final long presentationTimestampUs;
 
     /**
      * Creates a MotionPhotoInfo object associated with a given file.
      */
     @RequiresApi(api = Build.VERSION_CODES.M)
-    private MotionPhotoInfo(MediaFormat mediaFormat,
-                            int videoOffset,
-                            long presentationTimestampUs) {
+    private MotionPhotoInfo(MediaFormat mediaFormat, int videoOffset) {
         width = mediaFormat.getInteger(MediaFormat.KEY_WIDTH);
         height = mediaFormat.getInteger(MediaFormat.KEY_HEIGHT);
         duration = mediaFormat.getLong(MediaFormat.KEY_DURATION);
@@ -45,7 +56,6 @@ public class MotionPhotoInfo {
                 ? mediaFormat.getInteger(MediaFormat.KEY_ROTATION)
                 : 0;
         this.videoOffset = videoOffset;
-        this.presentationTimestampUs = presentationTimestampUs;
     }
 
     /**
@@ -82,16 +92,87 @@ public class MotionPhotoInfo {
     static MotionPhotoInfo newInstance(File file, MediaExtractor extractor)
             throws IOException, XMPException {
         XMPMeta meta = getFileXmp(file);
-        int videoOffset = meta.getPropertyInteger(
-                CAMERA_XMP_NAMESPACE,
-                "MicroVideoOffset"
-        );
-        long presentationTimestampUs = meta.getPropertyLong(
-                CAMERA_XMP_NAMESPACE,
-                "MicroVideoPresentationTimestampUs"
-        );
+        int videoOffset = getVideoOffset(meta);
         MediaFormat mediaFormat = getFileMediaFormat(file, extractor, videoOffset);
-        return new MotionPhotoInfo(mediaFormat, videoOffset, presentationTimestampUs);
+        return new MotionPhotoInfo(mediaFormat, videoOffset);
+    }
+
+    /**
+     * Finds the video offset encoded in the motion photo XMP metadata.
+     * @param meta The XMP metadata for the motion photo file.
+     * @return the number of bytes from the end of the motion photo file to the beginning of the
+     * video track.
+     * @throws XMPException when parsing invalid XMP metadata.
+     */
+    private static int getVideoOffset(XMPMeta meta) throws XMPException {
+        int version = getMotionPhotoVersion(meta);
+        int videoOffset = 0;
+        switch (version) {
+            case 1:
+                videoOffset = meta.getPropertyInteger(
+                        CAMERA_XMP_NAMESPACE,
+                        "MicroVideoOffset"
+                );
+                break;
+            case 2:
+                // Iterate through the nodes of the XMP metadata to find the desired item length
+                // and padding properties. The items we are looking for belong in an array with name
+                // "Directory" that is indexed starting at 1. We ignore the first item in the array
+                // because it is the primary item, and the video track is always stored immediately
+                // after the primary item (and any padding).
+                XMPIterator itr = meta.iterator();
+                int arrayItemIdx = 2;
+                while (itr.hasNext()) {
+                    XMPPropertyInfo property = (XMPPropertyInfo) itr.next();
+                    String propertyPath = property.getPath();
+                    String lengthProperty = "Container:Directory["
+                            + arrayItemIdx
+                            + "]/Container:Item/Item:Length";
+                    String paddingProperty = "Container:Directory["
+                            + arrayItemIdx
+                            + "]/Container:Item/Item:Padding";
+                    if (propertyPath != null) {
+                        if (propertyPath.equalsIgnoreCase(lengthProperty)) {
+                            videoOffset += Integer.parseInt(property.getValue());
+                        } else if (propertyPath.equalsIgnoreCase(paddingProperty)) {
+                            videoOffset += Integer.parseInt(property.getValue());
+                        }
+                    }
+                }
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid file format");
+        }
+        return videoOffset;
+    }
+
+    /**
+     * Get the version of the motion photo file encoded by this XMP metadata. There are only two
+     * possible versions, v1 or v2.
+     * @param meta The XMPMeta corresponding to the motion photo file in question.
+     * @return 1 if the version is v1, 2 if the version is v2, and 0 otherwise.
+     * @throws XMPException when parsing invalid XMP metadata.
+     */
+    private static int getMotionPhotoVersion(XMPMeta meta) throws XMPException {
+        if (meta.doesPropertyExist(CAMERA_XMP_NAMESPACE, "MicroVideo")) {
+            // This is microvideo version file
+            int microVideo = meta.getPropertyInteger(CAMERA_XMP_NAMESPACE, "MicroVideo");
+            if (microVideo == 1) {
+                return 1;
+            } else {
+                return 0;
+            }
+        } else if (meta.doesPropertyExist(CAMERA_XMP_NAMESPACE, "MotionPhoto")) {
+            // This is a motion photo version file
+            int motionPhoto = meta.getPropertyInteger(CAMERA_XMP_NAMESPACE, "MotionPhoto");
+            if (motionPhoto == 1) {
+                return 2;
+            } else {
+                return 0;
+            }
+        } else {
+            return 0;
+        }
     }
 
     /**
@@ -142,9 +223,5 @@ public class MotionPhotoInfo {
 
     public int getRotation() {
         return rotation;
-    }
-
-    public long getPresentationTimestampUs() {
-        return presentationTimestampUs;
     }
 }
