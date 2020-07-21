12d5e074ef3dd06ba4cad7a542b597dd9f298cb7
sxzhang25 <zsharonx@gmail.com>
2020-07-21 16:18:56 -0400

    Add OpenGL pipeline setup files
    - OutputSurface sets up (1) an EGLSurface for rendering based on the provided native surface that is passed to MotionPhotoReader; (2) a surface texture and corresponding surface for the media codec to decode
    - TextureRender sets up a renderer to write the image onto the coordinate system (as a texture mapped onto a single rectangular plane represented by two triangles)
    - Add basic texture rendering vertex and fragment shaders

--- MotionPhotoReader/app/src/main/java/com/google/android/libraries/motionphotoreader/OutputSurface.java
+++ MotionPhotoReader/app/src/main/java/com/google/android/libraries/motionphotoreader/OutputSurface.java
@@ -5,12 +5,19 @@ import android.opengl.EGL14;
 import android.opengl.EGLConfig;
 import android.opengl.EGLContext;
 import android.opengl.EGLDisplay;
+import android.opengl.EGLExt;
 import android.opengl.EGLSurface;
+import android.opengl.GLES20;
+import android.opengl.GLES30;
+import android.opengl.GLUtils;
 import android.os.Build;
 import android.util.Log;
 import android.view.Surface;
 
 import androidx.annotation.RequiresApi;
+
+import java.nio.IntBuffer;
+import java.util.Objects;
 
 public class OutputSurface implements SurfaceTexture.OnFrameAvailableListener {
     private static final String TAG = "OutputSurface";
@@ -19,80 +26,117 @@ public class OutputSurface implements SurfaceTexture.OnFrameAvailableListener {
     private EGLDisplay eglDisplay;
     private EGLContext eglContext;
     private EGLSurface eglSurface;
+    private EGLConfig eglConfig;
 
-    private Surface surface;
+    private int surfaceTextureHandle;
     private SurfaceTexture surfaceTexture;
+    private TextureRender textureRender;
+    private Surface surface;
     private boolean frameAvailable;
 
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
     public OutputSurface(int width, int height) {
         if (width <= 0 || height <= 0) {
             throw new IllegalArgumentException("Invalid surface dimensions");
         }
+
+        setup();
+        eglSetup(width, height);
+    }
+
+    private void setup() {
+        textureRender = new TextureRender();
+        textureRender.initializeStatePostSurfaceCreated();
+        createAndBindMotionPhotoTexture();
     }
 
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
     private void eglSetup(int width, int height) {
+        // Initialize EGL display
         eglDisplay = EGL14.eglGetDisplay(EGL14.EGL_DEFAULT_DISPLAY);
         if (eglDisplay == EGL14.EGL_NO_DISPLAY) {
             throw new RuntimeException("Unable to get EGL14 display");
         }
+
+        // Initialize EGL
         int[] version = new int[2];
         if (!EGL14.eglInitialize(eglDisplay, version, 0, version, 1)) {
             eglDisplay = null;
             throw new RuntimeException("Unable to initialize EGL14");
         }
 
-        // Configure EGL for pbuffer and OpenGL ES 2.0
-        int[] attribList = {
+        // Configure EGL
+        int[] numConfigs = new int[1];
+        EGLConfig[] configs = new EGLConfig[1];
+        int[] attributes = {
                 EGL14.EGL_RED_SIZE, 8,
                 EGL14.EGL_GREEN_SIZE, 8,
                 EGL14.EGL_BLUE_SIZE, 8,
                 EGL14.EGL_RENDERABLE_TYPE, EGL14.EGL_OPENGL_ES2_BIT,
-                EGL14.EGL_SURFACE_TYPE, EGL14.EGL_PBUFFER_BIT,
                 EGL14.EGL_NONE
         };
-        EGLConfig[] configs = new EGLConfig[1];
-        int[] numConfigs = new int[1];
         if (!EGL14.eglChooseConfig(
                 eglDisplay,
-                attribList, /* attrib_listOffset = */0,
+                attributes, /* attrib_listOffset = */0,
                 configs, /* configsOffset = */0,
                 configs.length, numConfigs, /* num_configOffset = */ 0
         )) {
             throw new RuntimeException("Unable to find RGB888+recordable ES2 EGL config");
+        } else if (numConfigs[0] == 0) {
+            throw new IllegalArgumentException("Could not find suitable EGLConfig");
         }
+        eglConfig = configs[0];
 
-        // Configure context for OpenGL ES 2.0
-        int[] attrib_list = {
-            EGL14.EGL_CONTEXT_CLIENT_VERSION, 2, EGL14.EGL_NONE
-        };
+        // Initialize EGL
         eglContext = EGL14.eglCreateContext(
                 eglDisplay,
-                configs[0],
+                eglConfig,
                 EGL14.EGL_NO_CONTEXT,
-                attrib_list,
+                new int[] {
+                        EGL14.EGL_CONTEXT_CLIENT_VERSION, 3,
+                        EGL14.EGL_NONE
+                },
                 /* offset = */ 0
         );
-        checkEglError("eglCreateContext");
+
+        // Make context current (with EGL_NO_SURFACE)
+        eglSurface = EGL14.EGL_NO_SURFACE;
+        if (!EGL14.eglMakeCurrent(eglDisplay, eglSurface, eglSurface, eglContext)) {
+            Log.e(TAG, "Failed to make context current");
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public void setSurface(Surface surface) {
+        this.surface = surface;
+
+        // Abort if context is null
         if (eglContext == null) {
-            throw new RuntimeException("Null EGL context");
+            Log.i(TAG, "EGL Context is null, can't set EGL surface");
+            return;
         }
 
-        // Create a pbuffer surface
-        int[] surfaceAttribs = {
-                EGL14.EGL_WIDTH, width,
-                EGL14.EGL_HEIGHT, height,
-                EGL14.EGL_NONE
-        };
-        eglSurface = EGL14.eglCreatePbufferSurface(
-                eglDisplay, 
-                configs[0], 
-                surfaceAttribs, 
-                /* offset = */ 0
-        );
-        checkEglError("eglCreatePbufferSurface");
-        if (eglSurface == null) {
-            throw new RuntimeException("EGL Surface is null");
+        // Destroy EGL surface if valid
+        if (!Objects.equals(eglSurface, EGL14.EGL_NO_SURFACE)) {
+            EGL14.eglDestroySurface(eglDisplay, eglSurface);
+        }
+
+        // Initialize EGL surface
+        if (surface == null) {
+            eglSurface = EGL14.EGL_NO_SURFACE;
+        } else {
+            eglSurface = EGL14.eglCreateWindowSurface(
+                    eglDisplay,
+                    eglConfig,
+                    surface,
+                    new int[] { EGL14.EGL_NONE },
+                    /* offset = */ 0
+            );
+        }
+
+        // Make EGL surface current
+        if (!EGL14.eglMakeCurrent(eglDisplay, eglSurface, eglSurface, eglContext)) {
+            Log.e(TAG, "Failed to make context current");
         }
     }
 
@@ -104,14 +148,13 @@ public class OutputSurface implements SurfaceTexture.OnFrameAvailableListener {
             EGL14.eglReleaseThread();
             EGL14.eglTerminate(eglDisplay);
         }
-        surface.release();
 
         eglDisplay = EGL14.EGL_NO_DISPLAY;
         eglContext = EGL14.EGL_NO_CONTEXT;
         eglSurface = EGL14.EGL_NO_SURFACE;
 
-        // textureRender = null;
-        surface = null;
+        textureRender = null;
+        surfaceTexture.release();
         surfaceTexture = null;
     }
 
@@ -122,8 +165,8 @@ public class OutputSurface implements SurfaceTexture.OnFrameAvailableListener {
         }
     }
 
-    public Surface getSurface() {
-        return surface;
+    public SurfaceTexture getSurfaceTexture() {
+        return surfaceTexture;
     }
 
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
@@ -132,6 +175,21 @@ public class OutputSurface implements SurfaceTexture.OnFrameAvailableListener {
         if ((error = EGL14.eglGetError()) != EGL14.EGL_SUCCESS) {
             throw new RuntimeException(msg + ": EGL error: 0x" + Integer.toHexString(error));
         }
+    }
+
+    private void createAndBindMotionPhotoTexture() {
+        IntBuffer frameBufferIds = IntBuffer.allocate(1);
+        GLES30.glGenFramebuffers(1, frameBufferIds);
+
+        IntBuffer textureIds = IntBuffer.allocate(1);
+        GLES30.glGenTextures(1, textureIds);
+
+        // Texture for motion photo outputs
+        surfaceTextureHandle = textureIds.get(0);
+
+        // After the motion photo texture has been created, the motion photo surface can be
+        // initialized
+        surfaceTexture = new SurfaceTexture(surfaceTextureHandle);
     }
 
     public void awaitNewImage() {
@@ -152,12 +210,12 @@ public class OutputSurface implements SurfaceTexture.OnFrameAvailableListener {
         }
 
         // Latch the data
-        // textureRender.checkGlError("before updateTexImage");
+        textureRender.checkGlError("before updateTexImage");
         surfaceTexture.updateTexImage();
     }
 
     public void drawImage() {
-        // textureRender.drawFrame(surfaceTexture);
+        textureRender.drawFrame(surfaceTexture);
     }
 
     @Override
--- MotionPhotoReader/app/src/main/java/com/google/android/libraries/motionphotoreader/TextureRender.java
+++ MotionPhotoReader/app/src/main/java/com/google/android/libraries/motionphotoreader/TextureRender.java
@@ -1,49 +1,24 @@
 package com.google.android.libraries.motionphotoreader;
 
 import android.graphics.SurfaceTexture;
-import android.media.MediaCodec;
-import android.media.MediaExtractor;
-import android.opengl.GLUtils;
+import android.opengl.GLES11Ext;
+import android.opengl.GLES20;
 import android.opengl.Matrix;
 import android.util.Log;
-import android.view.Surface;
 
-import java.io.File;
-import java.nio.IntBuffer;
-
-import static android.opengl.GLES11Ext.GL_TEXTURE_EXTERNAL_OES;
-import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
-import static android.opengl.GLES20.GL_COMPILE_STATUS;
-import static android.opengl.GLES20.GL_DEPTH_BUFFER_BIT;
-import static android.opengl.GLES20.GL_FRAGMENT_SHADER;
-import static android.opengl.GLES20.GL_INFO_LOG_LENGTH;
-import static android.opengl.GLES20.GL_LINK_STATUS;
-import static android.opengl.GLES20.GL_NO_ERROR;
-import static android.opengl.GLES20.GL_TEXTURE0;
-import static android.opengl.GLES20.GL_TEXTURE_WRAP_T;
-import static android.opengl.GLES20.GL_TRUE;
-import static android.opengl.GLES20.GL_VERTEX_SHADER;
-import static android.opengl.GLES20.glActiveTexture;
-import static android.opengl.GLES20.glAttachShader;
-import static android.opengl.GLES20.glBindTexture;
-import static android.opengl.GLES20.glClear;
-import static android.opengl.GLES20.glClearColor;
-import static android.opengl.GLES20.glCompileShader;
-import static android.opengl.GLES20.glCreateProgram;
-import static android.opengl.GLES20.glCreateShader;
-import static android.opengl.GLES20.glDeleteProgram;
-import static android.opengl.GLES20.glGetError;
-import static android.opengl.GLES20.glGetProgramInfoLog;
-import static android.opengl.GLES20.glGetProgramiv;
-import static android.opengl.GLES20.glGetShaderInfoLog;
-import static android.opengl.GLES20.glGetShaderiv;
-import static android.opengl.GLES20.glLinkProgram;
-import static android.opengl.GLES20.glShaderSource;
-import static android.opengl.GLES20.glUseProgram;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
 
 public class TextureRender {
 
     private static final String TAG = "TextureRender";
+
+    private static final int FLOAT_SIZE_BYTES = 4; // number of bytes for a float?
+    private static final int TRIANGLE_VERTICES_DATA_POS_OFFSET = 0;
+    private static final int TRIANGLE_VERTICES_DATA_STRIDE_BYTES = 5 * FLOAT_SIZE_BYTES; // 5 due to five coordinate float values (x,y,z,u,v)?
+    private static final int TRIANGLE_VERTICES_DATA_UV_OFFSET = 3; // texture coords begin 3 offset units from array start (skip the position coordinates)
+
     private static final String VERTEX_SHADER = "attribute vec4 a_Position\n"
             + "void main() {\n"
             + "  gl_Position = a_Position;\n"
@@ -54,11 +29,29 @@ public class TextureRender {
             + "  gl_FragColor = u_Color;\n"
             + "}\n";
 
+    // A single plane comprised of two triangles to hold the image texture
+    private final float[] triangleVerticesData = {
+            // positions (x,y,z)    // texture coordinates (u,v)
+            -1.0f, -1.0f, 0.0f,     0.0f, 0.0f,  // bottom left
+             1.0f, -1.0f, 0.0f,     1,0f, 0.0f,  // bottom right
+            -1.0f,  1.0f, 0.0f,     0.0f, 1.0f,  // top left
+             1.0f,  1.0f, 0.0f,     1.0f, 1.0f,  // top right
+    };
+
     private float[] stMatrix;
     private int textureID;
     private int program;
+    private int aPositionHandle;
+    private int aTextureHandle;
+
+    private FloatBuffer triangleVertices;
 
     public TextureRender() {
+        triangleVertices = ByteBuffer
+                .allocateDirect(triangleVerticesData.length * FLOAT_SIZE_BYTES)
+                .order(ByteOrder.nativeOrder())
+                .asFloatBuffer();
+        triangleVertices.put(triangleVerticesData).position(/* newPosition = */ 0);
         Matrix.setIdentityM(stMatrix, /* smOffset = */ 0);
     }
 
@@ -66,83 +59,117 @@ public class TextureRender {
         return textureID;
     }
 
-    public void drawFrame(SurfaceTexture surfaceTexture) {
-        checkGlError("onDrawFrame start");
-        surfaceTexture.getTransformMatrix(stMatrix);
-
-        glClearColor(/* red = */ 0.0f, /* green = */ 0.0f, /* blue = */ 0.0f, /* alpha = */ 1.0f);
-        glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
-
-        glUseProgram(program);
-        checkGlError("glUseProgram");
-
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_EXTERNAL_OES, textureID);
-
-
+    public void initializeStatePostSurfaceCreated() {
+        program = createProgram(VERTEX_SHADER, FRAGMENT_SHADER);
     }
 
     private int createProgram(String vertexSource, String fragmentSource) {
         // Load the shaders
-        int vertexShader = loadShader(GL_VERTEX_SHADER, vertexSource);
+        int vertexShader = loadShader(GLES20.GL_VERTEX_SHADER, vertexSource);
         if (vertexShader == 0) {
             return 0;
         }
-        int fragmentShader = loadShader(GL_FRAGMENT_SHADER, fragmentSource);
+        int fragmentShader = loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentSource);
         if (fragmentShader == 0) {
             return 0;
         }
 
         // Create the program
-        int program = glCreateProgram();
+        int program = GLES20.glCreateProgram();
         if (program != 0) {
-            glAttachShader(program, vertexShader);
+            GLES20.glAttachShader(program, vertexShader);
             checkGlError("glAttachShader");
-            glAttachShader(program, fragmentShader);
+            GLES20.glAttachShader(program, fragmentShader);
             checkGlError("glAttachShader");
-            glLinkProgram(program);
+            GLES20.glLinkProgram(program);
             int[] linkStatus = new int[1];
-            glGetProgramiv(program, GL_LINK_STATUS, linkStatus, 0);
-            if (linkStatus[0] != GL_TRUE) {
-                String infoLog = glGetProgramInfoLog(program);
+            GLES20.glGetProgramiv(program, GLES20.GL_LINK_STATUS, linkStatus, 0);
+            if (linkStatus[0] != GLES20.GL_TRUE) {
+                String infoLog = GLES20.glGetProgramInfoLog(program);
                 Log.e(TAG, "Could not link program: " + infoLog);
-                glDeleteProgram(program);
+                GLES20.glDeleteProgram(program);
                 program = 0;
             }
         }
         return program;
     }
 
-    private void checkGlError(String tag) {
-        int error;
-        while ((error = glGetError()) != GL_NO_ERROR) {
-            throw new RuntimeException(tag+ ": glError " + error);
-        }
-    }
-
     private int loadShader(int type, String shaderSrc) {
         // Create the shader object
-        int shader = glCreateShader(type);
+        int shader = GLES20.glCreateShader(type);
         if (shader == 0) {
             return 0;
         }
 
         // Load the shader source, compile the shader
-        glShaderSource(shader, shaderSrc);
-        glCompileShader(shader);
+        GLES20.glShaderSource(shader, shaderSrc);
+        GLES20.glCompileShader(shader);
 
         // check the compile status
         int[] compiled = new int[1];
-        glGetShaderiv(shader, GL_COMPILE_STATUS, compiled, 0);
+        GLES20.glGetShaderiv(shader, GLES20.GL_COMPILE_STATUS, compiled, 0);
         if (compiled[0] == 0) {
             // Retrieve the compiler messages when compilation fails
             int[] infoLen = new int[0];
-            glGetShaderiv(shader, GL_INFO_LOG_LENGTH, infoLen, 0);
+            GLES20.glGetShaderiv(shader, GLES20.GL_INFO_LOG_LENGTH, infoLen, 0);
             if (infoLen[0] > 1) {
-                String infoLog = glGetShaderInfoLog(shader);
+                String infoLog = GLES20.glGetShaderInfoLog(shader);
                 Log.d(TAG, infoLog);
             }
         }
         return shader;
+    }
+
+    public void drawFrame(SurfaceTexture surfaceTexture) {
+        checkGlError("onDrawFrame start");
+        surfaceTexture.getTransformMatrix(stMatrix);
+
+        GLES20.glClearColor(/* red = */ 0.0f, /* green = */ 0.0f, /* blue = */ 0.0f, /* alpha = */ 1.0f);
+        GLES20.glClear(GLES20.GL_DEPTH_BUFFER_BIT | GLES20.GL_COLOR_BUFFER_BIT);
+
+        GLES20.glUseProgram(program);
+        checkGlError("glUseProgram");
+
+        GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
+        GLES20.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, textureID);
+
+        // Process vertices
+        triangleVertices.position(TRIANGLE_VERTICES_DATA_POS_OFFSET);
+        GLES20.glVertexAttribPointer(
+                aPositionHandle,
+                /* size = */ 3,
+                /* type = */ GLES20.GL_FLOAT,
+                /* normalized = */ false,
+                TRIANGLE_VERTICES_DATA_STRIDE_BYTES, triangleVertices
+        );
+        checkGlError("glVertexAttribPointer aPosition");
+        GLES20.glEnableVertexAttribArray(aPositionHandle);
+        checkGlError("glEnableVertexAttribArray aPositionHandle");
+
+        // Process texture
+        triangleVertices.position(TRIANGLE_VERTICES_DATA_UV_OFFSET);
+        GLES20.glVertexAttribPointer(
+                aTextureHandle,
+                /* size = */ 2,
+                /* type = */ GLES20.GL_FLOAT,
+                /* normalized = */ false,
+                TRIANGLE_VERTICES_DATA_STRIDE_BYTES, triangleVertices
+        );
+        checkGlError("glVertexAttribPointer aTextureHandle");
+        GLES20.glEnableVertexAttribArray(aTextureHandle);
+        checkGlError("glEnableVertexAttribArray aTextureHandle");
+
+        // Do matrix stuff here...
+
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, /* first = */ 0, /* count = */ 4);
+        checkGlError("glDrawArrays");
+        GLES20.glFinish();
+    }
+
+    public void checkGlError(String tag) {
+        int error;
+        while ((error = GLES20.glGetError()) != GLES20.GL_NO_ERROR) {
+            throw new RuntimeException(tag+ ": glError " + error);
+        }
     }
 }
