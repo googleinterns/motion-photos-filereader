c5f165886e759ed0adc74ab65e72819a1cbab136
sxzhang25 <zsharonx@gmail.com>
2020-07-31 11:58:08 -0400

    Upload additional v1 test files.

--- MotionPhotoReader/app/src/main/java/com/google/android/libraries/motionphotoreader/WidgetTestActivity.java
+++ MotionPhotoReader/app/src/main/java/com/google/android/libraries/motionphotoreader/WidgetTestActivity.java
@@ -31,7 +31,7 @@ public class WidgetTestActivity extends Activity {
             motionPhotoWidget.setFile(
                     ResourceFetcher.fetchRawFile(
                             this.getApplicationContext(),
-                            R.raw.v1,
+                            R.raw.v1a,
                             "motionPhotoV1", "jpg"
                     )
             );
@@ -73,7 +73,7 @@ public class WidgetTestActivity extends Activity {
                     motionPhotoWidget.setFile(
                             ResourceFetcher.fetchRawFile(
                                     this.getApplicationContext(),
-                                    R.raw.v1,
+                                    R.raw.v1a,
                                     /* prefix = */ "motionPhotoV1",
                                     /* suffix = */ ".jpg"
                             )
